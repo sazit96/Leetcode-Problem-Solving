6d3b4f5d8320ff75d127239fe8a4a03cd83e771d
Ehosanul Islam Sazit <sazitislam96@gmail.com>
2024-05-29 06:15:03 +0600

    Today i solve another recurtion problem the problme was Fibonacci number

---
+++ Recursion/FibonacciNumber.cpp
@@ -0,0 +1,6 @@
+int fibo(int n)
+{
+    if (n <= 1)
+        return n;
+    return fibo(n - 1) + fibo(n - 2);
+}
---
+++ Recursion/FibonacciNumber.js
@@ -0,0 +1,4 @@
+function fibo(n) {
+  if (n <= 1) return n;
+  return fibo(n - 1) + fibo(n - 2);
+}
